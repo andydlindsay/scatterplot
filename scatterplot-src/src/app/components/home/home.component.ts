47f248993c9a8b391deeaefc86f60b0e30c27097
Andy Lindsay <andydlindsay@gmail.com>
2017-05-04 16:27:19 -0700

    adds dist for heroku deployment

--- scatterplot-src/src/app/components/home/home.component.ts
+++ scatterplot-src/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { D3Service, D3, Selection } from 'd3-ng2-service';
 import { Title } from '@angular/platform-browser';
 import { DataService } from '../../services/data.service';
+import { legendColor } from 'd3-svg-legend';
 
 @Component({
   selector: 'app-home',
@@ -20,6 +21,7 @@ export class HomeComponent implements OnInit {
     private data: DataService
   ) {
     this.d3 = d3Service.getD3();
+    // this.d3.legendColor = legendColor;
   }
 
   ngOnInit() {
@@ -34,17 +36,17 @@ export class HomeComponent implements OnInit {
         }
       }
     );
-    
+
   }
 
   convertSecondsToMinutes(timeInSeconds) {
-    const returnString = String(Math.floor(Number(timeInSeconds) / 60)) + ":" + this.zeroPad(String(Number(timeInSeconds) % 60));
+    const returnString = String(Math.floor(Number(timeInSeconds) / 60)) + ':' + this.zeroPad(String(Number(timeInSeconds) % 60));
     return returnString;
   }
 
   zeroPad(numToBePadded) {
-    if (numToBePadded.length == 1) {
-      return "0" + numToBePadded;
+    if (numToBePadded.length === 1) {
+      return '0' + numToBePadded;
     } else {
       return numToBePadded;
     }
@@ -52,11 +54,11 @@ export class HomeComponent implements OnInit {
 
   drawScatterplot() {
     // alias d3
-    let d3 = this.d3;
+    const d3 = this.d3;
 
     // setup svg component
     const width = 1000,
-          height = 750,
+          height = 650,
           padding = 60;
 
     // append svg component
@@ -70,15 +72,17 @@ export class HomeComponent implements OnInit {
     const fastestTime = this.dataset[0]["Seconds"];
     const slowestTime = d3.max(this.dataset, (d) => d["Seconds"]);
     const timeDiff = Number(slowestTime) - Number(fastestTime);
-    // console.log("timeDiff:", this.convertSecondsToMinutes(timeDiff));
+    const lastPlace = Number(d3.max(this.dataset, (d) => d["Place"]));
 
+    // add/subtract from domain to extend graph beyond data points
     const xScale = d3.scaleLinear()
-      .domain([timeDiff, 0])
+      .domain([timeDiff + 5, -5])
       .range([padding, width - padding]);
 
+    // add/subtract from domain to extend graph beyond data points
     const yScale = d3.scaleLinear()
-      .domain([1, 35])
-      .range([height - padding, padding]);
+      .domain([0, 36])
+      .range([height - padding, 0]);
 
     // x axis
     const xAxis = d3.axisBottom(xScale);
@@ -86,6 +90,8 @@ export class HomeComponent implements OnInit {
       .attr("transform", "translate(0, " + (height - padding) + ")")
       .call(xAxis);
 
+    // axis label based on d3noob's block
+    // https://bl.ocks.org/d3noob/23e42c8f67210ac6c678db2cd07a747e
     // add label to x axis
     svg.append("text")
       .attr("transform", "translate(" + (width / 2) + ", " + (height - 20) + ")")
@@ -102,13 +108,69 @@ export class HomeComponent implements OnInit {
     svg.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 15)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 60 - (height / 2))
       .attr("dy", "1em")
       .attr("text-anchor", "middle")
-      .text("Ranking");
+      .text("Rider Ranking");
+
+    // tooltip
+    const div = d3.select("#svg")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
     // add plots
-    
+    svg.selectAll("circle")
+      .data(this.dataset)
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => xScale(Number(d["Seconds"]) - fastestTime) )
+      .attr("cy", (d) => yScale(d["Place"]) )
+      .attr("r", 5)
+      .attr("fill", (d) => {
+        if (d["Doping"].length > 0) {
+          return "red";
+        } else {
+          return "blue";
+        }
+      })
+      .on('mouseover', (d) => {
+        div.transition()
+          .duration(100)
+          .style('opacity', 1);
+        div.html('<h2>'
+          + d['Name']
+          + ' (' + d['Nationality']
+          + ')</h2><h3>Time Behind Fastest: '
+          + this.convertSecondsToMinutes(d['Seconds'] - fastestTime)
+          + '</h3><h3>Year: '
+          + d['Year']
+          + ', Time: '
+          + d['Time']
+          + '</h3><h3>'
+          + d['Doping']
+          + '</h3>');
+      })
+      .on("mouseout", (d) => {
+        div.transition()
+          .duration(750)
+          .style("opacity", 0);
+      });
+
+    // legend
+    const ordinal = d3.scaleOrdinal()
+      .domain(["Riders with doping allegations", "No doping allegations"])
+      .range(["red", "blue"]);
+
+    svg.append("g")
+      .attr("class", "legendQuant")
+      .attr("transform", "translate(" + String(width - 350) + ", 280)");
+
+    const colorLegend = legendColor()
+      .scale(ordinal);
+
+    svg.select(".legendQuant")
+      .call(colorLegend);
 
   }
 
